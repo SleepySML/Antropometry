4084363d53ff440c31fe0c2dd900cd8488db2349
Eugene Kurdakov <evgenij.sleepy@gmali.com>
2017-07-27 18:34:18 +0300

    svg on footer, fixes on ther pages

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './slider.scss';
 //import img1 from '../../static/images/sliderImages/slide1.jpg';
-//import img1 from '../../static/images/sliderImages/slide1.png';
-//import img2 from '../../static/images/sliderImages/slide2.png';
-//import img3 from '../../static/images/sliderImages/slide3.png';
+import img1 from '../../static/images/sliderImages/slide1.png';
+import img2 from '../../static/images/sliderImages/slide2.png';
+import img3 from '../../static/images/sliderImages/slide3.png';
 
 const style = {
     transition: 'all .3s ease-out'
@@ -55,9 +55,9 @@ class Slider extends React.Component{
         return(
             <div className="slider-wrapper">
                 <ul className="Slider-list" style={{...style, marginLeft: this.state.marginLeft+"%"}}>
-                    <li className="slide"><img  alt="image1"/></li>
-                    <li className="slide"><img  alt="image2"/></li>
-                    <li className="slide"><img  alt="image3"/></li>
+                    <li className="slide"><img  src={img1} alt="image1"/></li>
+                    <li className="slide"><img  src={img2} alt="image2"/></li>
+                    <li className="slide"><img  src={img3} alt="image3"/></li>
                 </ul>
                 <div className="orbit-controls">
                     <button className="orbit-next" onClick={this.onNextSlide}><span className="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
